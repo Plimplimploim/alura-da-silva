a6634de85fd9ba8bc365e42dbab9086cd005ec53
Gabriel O. Ischiavolini <116840511+Bielmonge@users.noreply.github.com>
2024-11-27 18:41:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Planetas',
+    'Qual é o maior planeta do Sistema Solar?',
+    'O maior planeta do Sistema Solar é Júpiter.'
+);
+
+criaCartao(
+    'Estrelas',
+    'O que é uma supernova?',
+    'Uma supernova é a explosão de uma estrela no final de sua vida, liberando enorme quantidade de energia.'
+);
+
+criaCartao(
+    'Galáxias',
+    'Qual é o nome da galáxia onde a Terra está localizada?',
+    'A Terra está localizada na galáxia chamada Via Láctea.'
+);
+
+criaCartao(
+    'Exploração Espacial',
+    'Quem foi o primeiro humano a viajar para o espaço?',
+    'O primeiro humano a viajar para o espaço foi Yuri Gagarin, em 1961.'
+);
